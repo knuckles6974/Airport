ea51694e9e758cf3f3dee5806fba6191fd20853f
knuckles6974 <knuckles6974@gmail.com>
2022-07-23 22:11:47 +0900

    Modify files

--- airport/info/urls.py
+++ airport/info/urls.py
@@ -1,6 +1,5 @@
-from django.contrib import admin
 from .views         import AirportInfoView
-from django.urls import path
+from django.urls    import path
 
 urlpatterns = [
     path('', AirportInfoView.as_view()),
